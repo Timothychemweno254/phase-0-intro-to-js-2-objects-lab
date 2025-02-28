3e6bd546b43b9c67bebb42e1eecc99621305cdc3
timothychemweno254 <timothy.kiprop@student.moringaschool.com>
2025-02-28 18:50:30 +0300

    my aray

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution in this file!
+let employee = {
+    name: "John Doe",
+    streetAddress: "123 Main St"
+  };
+
+  function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return { ...employee, [key]: value };
+  }
+
+  function deleteFromEmployeeByKey(employee, key) {
+    let newEmployee = { ...employee };
+    delete newEmployee[key];
+    return newEmployee;
+  }
+  
+  function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+  }
+  
+  function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value; // Mutates the original employee object
+    return employee;
+  }
+  
